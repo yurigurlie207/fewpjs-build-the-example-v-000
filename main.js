a6a79c2ff867d2c2b3e82dbc76b15b5ab0e4e1cb
Adrienne Yao <adrienne.yao@icloud.com>
2021-01-22 16:39:12 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
-
+ 
 // Your JavaScript code goes here!
 
 const EMPTY_HEART = '♡'
